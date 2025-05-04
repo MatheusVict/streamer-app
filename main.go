3bddfd9fb4c4a5a3b2d171bdd4ef495e597d6351
MatheusVict <Matheus.Henrique0724@jala.universitY>
2025-05-04 19:53:08 -0300

    feat: add auth route

--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/color"
+	"io"
+	"log"
 	"net/http"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/healthcheck", func(c echo.Context) error {
+		return c.String(http.StatusOK, "It's working baby")
+	})
+
+	e.POST("/auth", func(c echo.Context) error {
+		log.Default().Println("POST /auth")
+		body := c.Request().Body
+		defer body.Close()
+
+		fields, _ := io.ReadAll(body)
+		color.Println(string(fields))
 		return c.String(http.StatusOK, "It's working baby")
 	})
 
