88b10fda455f1542eec57c7eb4bb8ce145979d52
MatheusVict <Matheus.Henrique0724@jala.universitY>
2025-05-05 16:41:07 -0300

    chore: populating database

---
+++ scripts/V2_stream_keys_populate.sql
@@ -0,0 +1,5 @@
+INSERT INTO "Lives" ("name", "stream_key")
+VALUES ('alanzoka', '340cc389-128d-4d48-bdad-343b1ba1c0be');
+
+INSERT INTO "Lives" ("name", "stream_key")
+VALUES ('idk', '073cc5b1-f892-449e-a756-654f89b1e57f');
